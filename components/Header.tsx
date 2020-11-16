1e31d523d53fb250c186b60a0971006a52075782
Leandro Acquati <leandroacquati@gmail.com>
2020-11-16 05:08:44 -0300

    add desktop Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,20 +4,32 @@ import styles from './Header.module.sass'
 
 const Header = () => (
   <header className={styles.header}>
-    <nav>
-      <Link href="/">
-        <a>Home</a>
-      </Link>{' '}
-        |{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>{' '}
-        |{' '}
-      <Link href="/users">
-        <a>Users List</a>
-      </Link>{' '}
-        | <a href="/api/users">Users API</a>
-    </nav>
+    <div className={styles.container}>
+      <nav>
+        <div className={styles.links}>
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./next-js-logo.svg" alt="Next JS Site" className={styles.logo} />
+          </a>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/users">
+            <a>Users List</a>
+          </Link>
+          <a href="/api/users">Users API</a>
+        </div>
+      </nav>
+    </div>
   </header>
 )
 
